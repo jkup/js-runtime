51fca2cf7928f50f1674e54e9f363b73b927b7ad
Jon Kuperman <jon.kuperman@gmail.com>
2023-05-04 23:02:20 +0100

    hello world

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,22 @@
 use rusty_v8 as v8;
-use std::fs;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // Read the JavaScript file from disk
-    let filename = "path/to/your/javascript/file.js";
-    let source_code = fs::read_to_string(filename)?;
-
-    // Initialize the V8 engine
-    let platform = v8::new_default_platform()?;
+    let platform = v8::new_default_platform(0, false).make_shared();
     v8::V8::initialize_platform(platform);
     v8::V8::initialize();
-
-    // Create a new V8 isolate and scope
-    let mut isolate = v8::Isolate::new(Default::default());
-    let scope = &mut v8::HandleScope::new(&mut isolate);
-
-    // Create a new context and enter it
+    
+    let isolate = &mut v8::Isolate::new(Default::default());
+    
+    let scope = &mut v8::HandleScope::new(isolate);
     let context = v8::Context::new(scope);
-    let mut context_scope = v8::ContextScope::new(scope, context);
-
-    // Compile and run the JavaScript code
-    let source = v8::String::new(scope, &source_code).unwrap();
-    let script = v8::Script::compile(scope, context, source, None)?;
-    let result = script.run(scope, context)?;
-
-    // Convert the result to a Rust string and print it
-    let result_string = result.to_string(scope).unwrap();
-    println!("{}", result_string);
-
-    // Clean up the V8 engine
-    drop(context_scope);
-    drop(isolate);
-    v8::V8::dispose();
-    v8::V8::shutdown_platform();
-
+    let scope = &mut v8::ContextScope::new(scope, context);
+    
+    let code = v8::String::new(scope, "'Hello' + ' World!'").unwrap();
+    println!("javascript code: {}", code.to_rust_string_lossy(scope));
+    
+    let script = v8::Script::compile(scope, code, None).unwrap();
+    let result = script.run(scope).unwrap();
+    let result = result.to_string(scope).unwrap();
+    println!("result: {}", result.to_rust_string_lossy(scope));
     Ok(())
 }
